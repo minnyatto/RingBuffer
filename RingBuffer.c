585a5ccbe6f9f646827b62e5474ff266ac97ba14
minnyatto <48784308+minnyatto@users.noreply.github.com>
2019-09-04 19:42:50 +0900

    fix return value for each function

--- RingBuffer.c
+++ RingBuffer.c
@@ -1,24 +1,82 @@
+/*
+	Author: minnyatto@gmail.com
+	
+	how to use:
+	
+		1. Declare RingBuff, RingData like follow example
+				
+				typedef struct
+				{
+					int16_t 	ItemNum;
+					char		ItemData[64];
+					int16_t		ItemCount;
+				} myRingData_t;
+				
+				
+				RingBuff_t 		myRingBuffer;
+				myRingData_t	myRingData[32];
+				
+		2. Create RingBuffer
+		
+				RingBuffer_Create(&myRingBuffer, 
+									&myRingData[0], 
+									sizeof(myRingData[0]), 
+									sizeof(myRingData)/sizeof(myRingData[0]));
+				
+		3. Use
+			
+			myRingData_t itemData;
+			
+			ret = RingBuffer_Get(&myRingBuffer, (void *)&itemData);
+			
+			ret = RingBuffer_Set(&myRingBuffer, (void *)&itemData);	
+
+			//When you want to know there is new data in RingBuffer or not but you dont
+			want to get the new data, use follow function and see the return value
+			
+			ret = RingBuffer_CheckNewData(&myRingBuffer);
+*/
+
 #include "RingBuffer.h"
 
-
-
-void RingBuffer_Create(RingBuff_t *ring, void *ringdata, uint16_t itemSize, uint16_t ringSize)
+RingBuff_Return_e RingBuffer_Create(RingBuff_t *ring, void *ringdata, uint16_t itemSize, uint16_t ringSize)
 {
+	if (ring->State != RINGBUFFER_STATE_NULL)
+	{
+		return RINGBUFFER_CREATED;
+	}
+	
 	ring->RingData = ringdata;
 	ring->ItemSize = itemSize;
 	ring->RingSize = ringSize;
-	
+	ring->State	   = RINGBUFFER_STATE_NORMAL;
+
+	return RINGBUFFER_OK;
 }
 
-void RingBuffer_Reset(RingBuff_t *ring)
+RingBuff_Return_e RingBuffer_Reset(RingBuff_t *ring)
 {
+	if (ring->State == RINGBUFFER_STATE_NULL)
+	{
+		return RINGBUFFER_NG;
+	}
+	
 	ring->ItemSize = 0;
 	ring->RingSize = 0;
+	ring->State	   = RINGBUFFER_STATE_NORMAL;
+
+	return RINGBUFFER_OK;
 }
 
-uint16_t RingBuffer_Get(RingBuff_t *ring, void *res)
+RingBuff_Return_e RingBuffer_Get(RingBuff_t *ring, void *res)
 {
-	uint16_t ret = 0;
+	if (ring->State != RINGBUFFER_STATE_NORMAL)
+	{
+		return RINGBUFFER_NG;
+	}
+	
+	RingBuff_Return_e ret = RINGBUFFER_NODATA;
+
 	if (ring->ReadIdx != ring->WriteIdx)
 	{
 		memcpy((char *)res,(const char*)(ring->RingData + (ring->ReadIdx)*(ring->ItemSize)), (size_t)(ring->ItemSize));
@@ -27,25 +85,35 @@ uint16_t RingBuffer_Get(RingBuff_t *ring, void *res)
 		{
 			ring->ReadIdx = 0;
 		}
-		ret = 1;
+		ret = RINGBUFFER_NEWDATA;
 	}
 
 	return ret;
 }
 
-uint16_t RingBuffer_Set(RingBuff_t *ring, const void *data)
+RingBuff_Return_e RingBuffer_Set(RingBuff_t *ring, const void *data)
 {
+	if (ring->State != RINGBUFFER_STATE_NORMAL)
+	{
+		return RINGBUFFER_NG;
+	}
+	
 	memcpy((char *)(ring->RingData + (ring->WriteIdx)*(ring->ItemSize)), ( const char *)data, (size_t)(ring->ItemSize));
 	ring->WriteIdx = ring->WriteIdx + 1;
 	if (ring->WriteIdx >= ring->RingSize)
 	{
 		ring->WriteIdx = 0;
 	}
-	return 1;
+	return RINGBUFFER_OK;
 }
 
-void RingBuffer_SetReadBack(RingBuff_t *ring, uint8_t NumOfIdx)
+RingBuff_Return_e RingBuffer_SetReadBack(RingBuff_t *ring, uint8_t NumOfIdx)
 {
+	if (ring->State != RINGBUFFER_STATE_NORMAL)
+	{
+		return RINGBUFFER_NG;
+	}
+	
 	if (ring->ReadIdx < NumOfIdx)
 	{
 		ring->ReadIdx = ring->RingSize - NumOfIdx + ring->ReadIdx ;
@@ -54,16 +122,23 @@ void RingBuffer_SetReadBack(RingBuff_t *ring, uint8_t NumOfIdx)
 	{
 		ring->ReadIdx = ring->ReadIdx - NumOfIdx;
 	}
+
+	return RINGBUFFER_OK;
 }
 
-uint8_t RingBuffer_CheckNewData(RingBuff_t *ring)
+RingBuff_Return_e RingBuffer_CheckNewData(RingBuff_t *ring)
 {
+	if (ring->State != RINGBUFFER_STATE_NORMAL)
+	{
+		return RINGBUFFER_NG;
+	}
+	
 	if (ring->ReadIdx != ring->WriteIdx)
 	{
-		return 1;
+		return RINGBUFFER_NEWDATA;
 	}
 	else
 	{
-		return 0;
+		return RINGBUFFER_NODATA;
 	}
 }
--- RingBuffer.h
+++ RingBuffer.h
@@ -1,3 +1,8 @@
+/*
+	Author: minnyatto@gmail.com
+*/
+
+
 #ifndef _RINGBUFFER_H_
 #define _RINGBUFFER_H_
 
@@ -5,20 +10,40 @@
 #include <stdint.h>
 #include <string.h>
 
+
+typedef enum
+{
+	RINGBUFFER_STATE_NULL,
+	RINGBUFFER_STATE_NORMAL,
+	RINGBUFFER_STATE_ERROR,
+} RingBuff_State_e;
+
+typedef enum
+{
+	RINGBUFFER_NODATA,
+	RINGBUFFER_NEWDATA,
+	RINGBUFFER_OK,
+	RINGBUFFER_NG,
+	RINGBUFFER_CREATED,
+	
+} RingBuff_Return_e;
+	
+
 typedef struct
 {
-	void     *RingData;
-	uint16_t ItemSize;
-	uint16_t RingSize;
-	uint16_t ReadIdx;
-	uint16_t WriteIdx;
+	void     				*RingData;
+	uint16_t 				ItemSize;
+	uint16_t 				RingSize;
+	uint16_t 				ReadIdx;
+	uint16_t 				WriteIdx;
+	RingBuff_State_e		State;
 	
 } RingBuff_t;
 
-void RingBuffer_Create(RingBuff_t *ring, void *ringdata, uint16_t itemSize, uint16_t ringSize);
-uint16_t RingBuffer_Get(RingBuff_t *ring, void *res);
-uint16_t RingBuffer_Set(RingBuff_t *ring, const void *data);
-void RingBuffer_SetReadBack(RingBuff_t *ring, uint8_t NumOfIdx);
-uint8_t RingBuffer_CheckNewData(RingBuff_t *ring);
+RingBuff_Return_e RingBuffer_Create(RingBuff_t *ring, void *ringdata, uint16_t itemSize, uint16_t ringSize);
+RingBuff_Return_e RingBuffer_Get(RingBuff_t *ring, void *res);
+RingBuff_Return_e RingBuffer_Set(RingBuff_t *ring, const void *data);
+RingBuff_Return_e RingBuffer_SetReadBack(RingBuff_t *ring, uint8_t NumOfIdx);
+RingBuff_Return_e RingBuffer_CheckNewData(RingBuff_t *ring);
 
 #endif
